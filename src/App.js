5975a83cb7917384bef434a7d84dbf7a99824a48
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-08-01 16:22:55 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// keys
+import { APP_ID } from './utils/api';
+import { APP_KEY } from './utils/api';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello Recipe App </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/api.js
@@ -0,0 +1,5 @@
+//import axios from 'axios';
+
+export const APP_ID = 'bd19d35e';
+
+export const APP_KEY = '0880f7a6ba7f6cba01766160d2135631';
