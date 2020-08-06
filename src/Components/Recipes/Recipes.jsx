7ea6cd6b1377eb8ab47b21b7320641c1e548327f
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-08-06 22:11:50 +0200

    Holds image, recipe details & ingredients for app

--- src/Components/Recipes/Recipes.jsx
+++ src/Components/Recipes/Recipes.jsx
@@ -1,21 +1,3 @@
-/*import React from 'react';
-import style from '../../recipe.module.css';
-
-export default function Recipes({ title, calories, image, ingredients }) {
-  return (
-    <div className={style.recipe}>
-      <h1>{title}</h1>
-      <ol>
-        {ingredients.map((ingredient, i) => (
-          <li key={i}>{ingredient.text}</li>
-        ))}
-      </ol>
-      <p>{calories}</p>
-
-      <img className={style.image} src={image} alt="" />
-    </div>
-  );
-} */
 import React, { useState } from 'react';
 // component
 import RecipeDetails from '../RecipeDetails/RecipeDetails';
