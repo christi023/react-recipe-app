a4460804a6b4fbe0a82d4a4a8f2a14066c625ce8
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-08-06 22:10:24 +0200

    Holds Recipes & Alert for food search app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+//import { uuid } from 'uuid/dist/v4';
 
 // Components
 import Recipes from './Components/Recipes/Recipes';
+import Alert from './Components/Alert/Alert';
 
 // keys
 import { APP_ID } from './utils/api';
@@ -9,65 +12,56 @@ import { APP_KEY } from './utils/api';
 
 import './App.css';
 
-function App() {
+export default function App() {
   // state
   const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState('');
-  const { query, setQuery } = useState('chicken');
+  const [alert, setAlert] = useState('');
+  const [query, setQuery] = useState('');
 
-  // useEffect
-  useEffect(() => {
-    getRecipes(); // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [query]);
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-  const getRecipes = async () => {
-    const response = await fetch(
-      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`,
-    );
-    const data = await response.json();
-    console.log(data.hits);
-    setRecipes(data.hits);
+  const getData = async () => {
+    if (query !== '') {
+      const result = await axios.get(url);
+      if (!result.data.more) {
+        return setAlert('No food with such name');
+      }
+      console.log(result);
+      setRecipes(result.data.hits);
+      setQuery('');
+      setAlert('');
+    } else {
+      setAlert('Please fill the form');
+    }
   };
 
-  // search method
-  const updateSearch = (e) => {
-    setSearch(e.target.value);
-  };
-
-  // get search method
-  const getSearch = (e) => {
+  // onSubmit method
+  const onSubmit = (e) => {
     e.preventDefault();
-    setQuery(search);
-    setSearch('');
+    getData();
+  };
+
+  // onChange method
+  const onChange = (e) => {
+    setQuery(e.target.value);
   };
 
   return (
     <div className="App">
-      <form onSubmit={getSearch} className="search-form">
+      <h1>Food Searching App</h1>
+      <form className="search-form" onSubmit={onSubmit}>
+        {alert !== '' && <Alert alert={alert} />}
         <input
-          className="search-input"
           type="text"
-          value={search}
-          //onChange={(e) => updateSearch(e)}
-          onChange={updateSearch}
+          placeholder="Search Food"
+          autoComplete="off"
+          onChange={onChange}
+          value={query}
         />
-        <button className="search-btn" type="submit">
-          Search
-        </button>
+
+        <input type="submit" value="search" />
       </form>
-      <div className="recipes">
-        {recipes.map((recipe, index) => (
-          <Recipes
-            key={index}
-            title={recipe.recipe.label}
-            calories={recipe.recipe.calories}
-            image={recipe.recipe.image}
-            ingredients={recipe.recipe.ingredients}
-          />
-        ))}
-      </div>
+      <div>{recipes !== [] && recipes.map((recipe, i) => <Recipes key={i} recipe={recipe} />)}</div>
     </div>
   );
 }
-
-export default App;
