fedcc2f6c54260e233e8a27d95a4e2bcb5f539d1
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-08-08 22:54:36 +0200

    initial commit for css

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,3 @@
-export const APP_ID = ''; // your key here
+export const APP_ID = 'bd19d35e'; // your key here
 
-export const APP_KEY = ''; // your key here
+export const APP_KEY = '0880f7a6ba7f6cba01766160d2135631	'; // your key here
